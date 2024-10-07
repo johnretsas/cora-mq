408ab9bb073cefc7bd0f3553dc698b32a1b4a00f
John Retsas <joretsas@gmail.com>
2024-10-07 15:18:09 +0100

    fix: comments for dequeing

--- queue/acknowledge.go
+++ queue/acknowledge.go
@@ -10,6 +10,10 @@ func (q *Queue) Acknowledge(id string) error {
 	defer q.mu.Unlock()
 
 	fmt.Println("Items in queue:")
+	// Scan the queue items and acknowledge the item with the given id
+	// Arguably, not the most efficient way to do this
+	// But assuming that the acknowledgement comes from the client fairly quickly after dequeuing
+	// The message to be acknowledged should be close to the beginning of the queue
 	for i := range q.items {
 		if q.items[i].ID == id {
 			q.items[i].Acknowledged = true
@@ -18,6 +22,10 @@ func (q *Queue) Acknowledge(id string) error {
 		q.items[i].PrettyPrint()
 	}
 
+	// Finding the id in the in-flight queue
+	// means that the item has been dequeued but not acknowledged
+	// and we need to remove it from the in-flight queue
+	// and increment the ackIndex
 	for i, item := range q.inFlight {
 		if item.ID == id {
 			q.inFlight = append(q.inFlight[:i], q.inFlight[i+1:]...)
--- queue/dequeue.go
+++ queue/dequeue.go
@@ -7,6 +7,7 @@ import (
 )
 
 // Dequeue retrieves the highest-priority visible item from the queue
+// The first visible item is returned to the client and it is considered in-flight
 func (q *Queue) Dequeue() (*QueueItem, error) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
@@ -21,19 +22,31 @@ func (q *Queue) Dequeue() (*QueueItem, error) {
 		// Pop an item from the heap
 		item := heap.Pop(q).(QueueItem)
 
-		// Skip acknowledged items
+		// Remove (Pop) and skip acknowledged items
 		if item.Acknowledged {
 			continue
 		}
 
 		// If item is not visible, add to tempList
+		// These are items that have been dequeued but not acknowledged
+		// In this case, we don't want to remove them from the heap
+		// because they will be re-added to the heap later if they are not acknowledged
 		if time.Now().Before(item.visibilityTime) {
 			tempList = append(tempList, item)
 			continue
 		}
 
+		// At this point, we have found a visible item
+		// The tempList contains all the items that are not visible yet
+
 		// Item is visible, update visibility timeout and add to in-flight list
+		// This item will become invisible for 10 seconds and it is considered in-flight
+		// The client will have 10 seconds to acknowledge the item
+		// If the item is not acknowledged within 10 seconds, it will be available again
 		item.visibilityTime = time.Now().Add(10 * time.Second)
+
+		// Add the item to the in-flight list
+		// This list is used to keep track of items that have been dequeued but not acknowledged
 		q.inFlight = append(q.inFlight, item)
 
 		// Add the item you've just dequeued to the tempList
@@ -42,18 +55,24 @@ func (q *Queue) Dequeue() (*QueueItem, error) {
 		// it will be available again
 		tempList = append(tempList, item)
 
-		// Re-add all temporary items back to the heap
+		// In order to find a visible item, we need to skip the items that are not visible
+		// These items have been popped and they should be re-added to the heap
 		for _, tempItem := range tempList {
 			heap.Push(q, tempItem)
 		}
 
+		// Return the visible item you have found.
 		return &item, nil
 	}
 
-	// Re-add the items back to the queue if none were visible
+	// At this point, we have gone through all the items in the queue and none were visible
+	// Re-add the items back to the queue if none were visible.
+	// So we have to re-add all the invisible items back to the heap
+	// and return an error
 	for _, item := range tempList {
 		heap.Push(q, item)
 	}
 
+	// If all items are invisible, return an error
 	return nil, errors.New("queue is empty or all items are invisible")
 }
